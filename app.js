f98e29c1ca837132c77516275a3293e4f2d5defb
Sjors van Heuveln <sjorsvanheuveln@Sjors-iMac.local>
2021-05-13 15:09:12 +0200

    minimal mongoose setup

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 //// HOT RELOADING ////////////////////////////////////////////
-var webpack = require('webpack');
-var webpackConfig = require('./webpack.config');
-var compiler = webpack(webpackConfig);
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+const compiler = webpack(webpackConfig);
 ///////////////////////////////////////////////////////////////
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+// Connect Mongoose //
+mongoose.connect('mongodb://localhost/express-react');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
