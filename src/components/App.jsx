f277459d732b491e96dc7dfe1d2b2d754e15def6
Sjors van Heuveln <sjorsvanheuveln@gmail.com>
2021-05-23 01:44:08 +0200

    Added Spinner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,28 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import LoginPage from './account/LoginPage';
 import ProfilePage from './account/ProfilePage';
 import HomePage from './home/HomePage';
 import Header from './shared/Header';
 
-const App = () => (
-  <Router>
-    <div className="wrapper">
-      <Header />
-      <section className="p-3">
-        <Route exact path="/"><HomePage /></Route>
-        <Route path="/login"><LoginPage /></Route>
-        <Route path="/profile"><ProfilePage /></Route>
-      </section>
-    </div>
-  </Router>
-);
-
-export default App;
+export default function App() {
+  const { progress } = useSelector((state) => state.progress);
+  
+  return (
+    <Router>
+      <div className="wrapper">
+        <Header />
+        <section className="p-3">
+          <Route exact path="/"><HomePage /></Route>
+          <Route path="/login"><LoginPage /></Route>
+          <Route path="/profile"><ProfilePage /></Route>
+        </section>
+        <div className="loader-wrapper" style={{ display: progress > 0 ? 'block' : 'none' }}>
+          <div className="loader-box">
+            <div className="loader">Loading ...</div>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
